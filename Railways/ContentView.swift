d7e09d9a4a68119515ea70eb4f23e1f14c794420
Terran Kroft <terrankroft@gmail.com>
2020-09-04 21:54:13 +0800

    Initial Commit

---
+++ Railways/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Railways
+//
+//  Created by Terran Kroft on 9/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Railways/RailwaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailwaysApp.swift
+//  Railways
+//
+//  Created by Terran Kroft on 9/4/20.
+//
+
+import SwiftUI
+
+@main
+struct RailwaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
